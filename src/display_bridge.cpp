80853f82cd1dfd2bfbe013e27b1bb23928b1e4b3
Adrian <swirszczadrian@o2.pl>
2022-10-03 15:36:17 +0200

    Fixed wiringPi imports

--- src/display_bridge.cpp
+++ src/display_bridge.cpp
@@ -27,10 +27,29 @@ int main() {
 
 
 #include <wiringPi.h>
+#include <lcd.h>
+#include <unistd.h>
+
+
+
+//USE WIRINGPI PIN NUMBERS
+#define LCD_RS  25               //Register select pin
+#define LCD_E   24               //Enable Pin
+#define LCD_D4  23               //Data pin 4
+#define LCD_D5  22               //Data pin 5
+#define LCD_D6  21               //Data pin 6
+#define LCD_D7  14               //Data pin 7
 
 
 int main() {
-
+    int lcd;               
+    wiringPiSetup();   
+       
+    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+    lcdClear(lcd);
+    
+    sleep(2);
+    lcdPuts(lcd, "Hello world!"); 
 }
 
 
--- src/imu_bridge.cpp
+++ src/imu_bridge.cpp
@@ -36,6 +36,8 @@ int main() {
 
 using namespace bno055;
 
+#define DEG_2_RAD 0.01745329251f
+
 /**
  * @brief Simple class for holding vecotr data
  * 
@@ -128,14 +130,15 @@ public:
              : node_name_(ImuBridge::rosInit("imu_bridge")) 
              , nh_(ros::NodeHandle())
              , read_rate_(100)  // This value depends on sensor's update rate. For Adafruit BNO055 it is 100 Hz
-             , loop_rate_(loop_rate)
-             , linear_calib_(0)
-             , angular_calib_(0)
-             , linear_filtered_()
-             , linear_prev_()
-             , angular_filtered_()
-             , angular_prev_()
-             , accel_prev_() {
+             , publish_rate_(loop_rate)
+             , accel_calib_(0U)
+             , gyro_calib_(0U)
+             , accel_()
+             , time_prev_(0.0f)
+             , velocity_()
+             , velocity_prev_()
+             , velocity_filtered_()
+             , rotation_()  {
         ROS_INFO("Starting up %s node:", node_name_.c_str());
 
         // Create service server, that receives requests from /imu_reset_variables topic and passes them to
@@ -188,60 +191,44 @@ public:
      * There is no need for user to call this function manually!
      * @param event Struct holding some basic time-oriented data, passed by ros
      */
-    void readSensorData(const ros::TimerEvent &event) {     
+    void readSensorData() {     
         // Read calibration data
         uint8_t sys_stat_calib = 0U;
         uint8_t mag_calib = 0U;
-        bno_ptr_->getCalibration(sys_stat_calib, angular_calib_, linear_calib_, mag_calib);
+        bno_ptr_->getCalibration(sys_stat_calib, gyro_calib_, accel_calib_, mag_calib);
 
-        // Create buffers to which data will be written into
-        float angular_buffer[3] = {0.0f, 0.0f, 0.0f};
-        float linear_buffer[3] = {0.0f, 0.0f, 0.0f};
-        float gravity_buffer[3] = {0.0f, 0.0f, 0.0f};
+        // Read rotation data and (if read succeeded) scale it accordingly
+        float rotation_buffer[3] = {0.0f, 0.0f, 0.0f};
+        if(bno_ptr_->getVector(rotation_buffer, VectorMappings::VECTOR_EULER)) {
+            // For rotation vector we need to assign correct values to correct variables and scale them from degrees to radians
+            rotation_.x = rotation_buffer[3] * DEG_2_RAD;
+            rotation_.y = rotation_buffer[2] * (-DEG_2_RAD);
+            rotation_.z = (rotation_buffer[1] - 180.0f) * (-DEG_2_RAD);
+        } // Otherwise we will use old data (so no action is necessary)
+        // Since Adafruit BNO055 sensor automatically filters data for Euler vector we don't need to use low pass filter
 
-        // Create variables to which scaled data will be written into
-        Vector angular = {};
-        Vector linear = {};
+        // Get current time
+        float time_curr = ros::Time::now().toSec();
 
-        // Read angular data. If read didn't succeeed, use data from previous loop
-        if (bno_ptr_->getVector(angular_buffer, VectorMappings::VECTOR_GYROSCOPE)) {
-            // For angular data we only need to assign correct values to correct variables
-            angular.x = -angular_buffer[0];
-            angular.y = angular_buffer[1];
-            angular.z = angular_buffer[2];
-        } else {
-            angular = angular_prev_;
-        }
-
-        // Read linear data. If read didn't succeeed, use data from previous loop
-        if (bno_ptr_->getVector(linear_buffer, VectorMappings::VECTOR_LINEARACCEL)) {
-            // For linear data we need to assign correct values to correct variables
+        // Read acceleration data and (if read succeeded) scale it
+        float accel_buffer[3] = {0.0f, 0.0f, 0.0f};
+        if (bno_ptr_->getVector(accel_buffer, VectorMappings::VECTOR_LINEARACCEL)) {
+            // For acceleration data we need to assign correct values to correct variables
             // Since we read linear acceleration data, we don't need to subtract gravity values (sensor does that for us)
-            linear.x = linear_buffer[0];
-            linear.y = -linear_buffer[1];
-            linear.z = -linear_buffer[2];
+            accel_.x = accel_buffer[0];
+            accel_.y = -accel_buffer[1];
+            accel_.z = -accel_buffer[2];
+        } // Otherwise we will use old data (so no action is necessary)
 
-            // Update previous acceleration
-            // This wasn't done in angular velocity, since back then we didn't need two vectors for storing old data
-            // (in contrast, here we need one vector for storing acceleration, in case we can't get a read from sensor, and one for storing velocity)
-            accel_prev_ = linear;
-        } else {
-            linear = accel_prev_;
-        }
-
-        // Caluclate the time between this and last loop
-        float time_diff = (event.current_real - event.last_real).toSec();
-
-        // Calculate velocity (typical discrete integral)
-        linear = (linear * time_diff) + linear_prev_;
+        // Calculate integral
+        velocity_ = accel_ * (time_curr - time_prev_) + velocity_prev_;    
 
         // Low pass filter
-        angular_filtered_ = angular_filtered_ * 0.63946f + angular * 0.18027f + angular_prev_ * 0.18027f;
-        linear_filtered_ = linear_filtered_ * 0.63946f + linear * 0.18027f + linear_prev_ * 0.18027f;
+        velocity_filtered_ = velocity_filtered_ * 0.63946f + velocity_ * 0.18027f + velocity_prev_ * 0.18027f;
 
-        // Update values
-        angular_prev_ = angular;
-        linear_prev_ = linear;
+        // Update remaining values
+        time_prev_ = time_curr;
+        velocity_prev_ = velocity_;
     }
 
     /**
@@ -249,23 +236,23 @@ public:
      * There is no need for user to call this function manually!
      * @param event Struct holding some basic time-oriented data, passed by ros
      */
-    void publishSensorData(const ros::TimerEvent &event) {
+    void publishSensorData() {
         // Create message and fill it with basic data
         tank_controller::imuData msg;
         msg.header.stamp = ros::Time::now();
 
         // Fill calibration data
-        msg.linearCalib = linear_calib_;
-        msg.angularCalib = angular_calib_;
+        msg.accelerometerCalib = accel_calib_;
+        msg.gyroscopeCalib = gyro_calib_;
 
         // Fill velocity data
-        msg.linear.x = linear_filtered_.x;
-        msg.linear.y = linear_filtered_.y;
-        msg.linear.z = linear_filtered_.z;
+        msg.linear.x = velocity_filtered_.x;
+        msg.linear.y = velocity_filtered_.y;
+        msg.linear.z = velocity_filtered_.z;
 
-        msg.angular.x = angular_filtered_.x;
-        msg.angular.y = angular_filtered_.y;
-        msg.angular.z = angular_filtered_.z;
+        msg.rotation.x = rotation_.x;
+        msg.rotation.y = rotation_.y;
+        msg.rotation.z = rotation_.z;
 
         // Publish message
         imu_data_pub_.publish(msg);
@@ -276,25 +263,26 @@ public:
      * @return EXIT_SUCCESS if closed properly
      */
     int run() {
-        // Since this node has to use two, different rates (one to read data from sensor and second to publish it) I've decided to use ros timers instead of typical "while" loop
-        // Timers call given function in (somewhat) regular time intervals and pass to them structs, containing basic time-orientet data (like current time and time of last call)
-        // Different timers can use different rates, making them perfect for this specific node
-
-        ROS_INFO("Starting up main program loop");
-
-        // Create timer, that calls function readSensorData, which is a method of ImuBridge class and belongs to "this" object
-        // Period between calls will be roughly equal to 1/read_rate_
-        read_timer_ = nh_.createTimer(ros::Duration(1/read_rate_), &ImuBridge::readSensorData, this);
-
-        // Create timer, that calls function publishSensorData, which is a method of ImuBridge class and belongs to "this" object
-        // Period between calls will be roughly equal to 1/loop_rate_
-        publish_timer_ = nh_.createTimer(ros::Duration(1/loop_rate_), &ImuBridge::publishSensorData, this);
+        ROS_INFO("Main program loop has been started");
 
         // Reset internal variables (juuust in case)
-        this->resetVariables_();
+        resetVariables_();
 
-        ROS_INFO("Main program loop has beem started");
-        ros::spin();    // Since everything is managed automatically there is no need for "while" loop
+        while (ros::ok()) {
+            // Read sensor data
+            // readSensorData();
+
+            // If enough time has passed publish message
+            // if (publish_rate_.)
+            for (int i=0; i<3; i++) {
+                std::cout << publish_rate_.cycleTime().toSec() <<" ";
+                read_rate_.sleep();
+            }
+            std::cout << std::endl;
+            publish_rate_.sleep();
+
+            ros::spinOnce();
+        }
 
         return EXIT_SUCCESS;
     }
@@ -313,77 +301,85 @@ private:
      * @brief Resets nodes internal variables (that is integrals, filters etc.)
      */
     void resetVariables_() {
-        linear_calib_ = 0U;
-        angular_calib_ = 0U;
+        // Reset calibration values
+        accel_calib_ = 0U;
+        gyro_calib_ = 0U;
 
-        linear_filtered_.setZero();
-        linear_prev_.setZero();
-        angular_filtered_.setZero();
-        angular_prev_.setZero();
+        // Reset velocity but keep timer running!
+        time_prev_ = ros::Time::now().toSec();
+        accel_.setZero();
+        velocity_.setZero();
+        velocity_prev_.setZero();
+        velocity_filtered_.setZero();
 
-        accel_prev_.setZero();
+        // No need to reset rotation data
     }
 
     std::string node_name_;
     
-    uint8_t linear_calib_;
-    uint8_t angular_calib_;
-    Vector linear_filtered_;
-    Vector linear_prev_;
-    Vector angular_filtered_;
-    Vector angular_prev_;
+    uint8_t accel_calib_;
+    uint8_t gyro_calib_;
     
-    Vector accel_prev_;
+    Vector accel_;
+    float time_prev_;
+    Vector velocity_;
+    Vector velocity_prev_;
+    Vector velocity_filtered_;
+
+    Vector rotation_;
 
     std::unique_ptr<ConnectionBridge> bno_ptr_;
 
     ros::NodeHandle nh_;
-    int read_rate_;
-    int loop_rate_;
+    ros::Rate read_rate_;
+    ros::Rate publish_rate_;
     ros::ServiceServer reset_vars_ser_;
     ros::Publisher imu_data_pub_;
-    ros::Timer read_timer_;
-    ros::Timer publish_timer_;
 };
 
 
 int main() {
-//     ImuBridge imu_bridge = {};
-//     return imu_bridge.run();
-// }
-
-    ConnectionBridge bno = {OperationMode::OPERATION_MODE_IMUPLUS};
-    bno.setExtCrystalUse(false);
-    float accel_vec[3] = {};
-    float gyro_vec[3] = {};
-    byte sys_stat = 0;
-    byte gyro = 0;
-    byte accel = 0;
-    byte mag = 0;
-    bool fully_calib = false;
-
-    while (true) {
-        bno.getVector(accel_vec, VectorMappings::VECTOR_LINEARACCEL);
-        bno.getVector(gyro_vec, VectorMappings::VECTOR_GYROSCOPE); 
-        // bno.getCalibration(sys_stat, gyro, accel, mag);
-        // fully_calib = bno.isFullyCalibrated();
-            std::cout << std::fixed << std::setw(7) << std::setprecision(2) 
-                      << "Accel:\n"
-                      << "  x = " << accel_vec[0] << "\n"
-                      << "  y = " << accel_vec[1] << "\n"
-                      << "  z = " << accel_vec[2] << "\n"
-                      << "Gyro:\n"
-                      << "  x = " << gyro_vec[0] << "\n"
-                      << "  y = " << gyro_vec[1] << "\n"
-                      << "  z = " << gyro_vec[2] << "\n" 
-                      << std::endl;
-                    //   << "Calib (" << (fully_calib ? "is" : "isn't") << " fully calibrated):\n"
-                    //   << "  acc = " << (int) accel << "\n"
-                    //   << "  gyr = " << (int) gyro << "\n"
-                    //   << "  mag = " << (int) mag << "\n" << std::endl;
-        delay(50);
-    }
+    ImuBridge imu_bridge = {};
+    return imu_bridge.run();
 }
+
+//     ConnectionBridge bno = {OperationMode::OPERATION_MODE_IMUPLUS};
+//     bno.setExtCrystalUse(true);
+//     float accel_vec[3] = {};
+//     float gyro_vec[3] = {};
+//     byte sys_stat = 0;
+//     byte gyro = 0;
+//     byte accel = 0;
+//     byte mag = 0;
+//     bool fully_calib = false;
+//     int errors = 0;
+
+//     while (true) {
+//         bno.getVector(accel_vec, VectorMappings::VECTOR_LINEARACCEL);
+//         bno.getVector(gyro_vec, VectorMappings::VECTOR_EULER); 
+//         if (fabs(accel_vec[0]) > 1.1f || fabs(gyro_vec[0]) > 7) {
+//             errors++;
+//         }
+//         // bno.getCalibration(sys_stat, gyro, accel, mag);
+//         // fully_calib = bno.isFullyCalibrated();
+//         std::cout << std::fixed << std::setw(7) << std::setprecision(2) 
+//                     << "Accel:\n"
+//                     << "  x = " << accel_vec[0] << "\n"
+//                     << "  y = " << accel_vec[1] << "\n"
+//                     << "  z = " << accel_vec[2] << "\n"
+//                     << "Gyro:\n"
+//                     << "  x = " << gyro_vec[0] << "\n"
+//                     << "  y = " << gyro_vec[1] << "\n"
+//                     << "  z = " << gyro_vec[2] << "\n"
+//                     << "Errors: " << errors <<"\n"
+//                     << std::endl;
+//                     // std::cout << "Calib (" << (fully_calib ? "is" : "isn't") << " fully calibrated):\n"
+//                     // << "  acc = " << (int) accel << "\n"
+//                     // << "  gyr = " << (int) gyro << "\n"
+//                     // << "  mag = " << (int) mag << "\n" << std::endl;
+//         delay(50);
+//     }
+// }
 
 
 #endif
